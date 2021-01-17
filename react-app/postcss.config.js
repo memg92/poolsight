326741406d83e5ccc9ac93ace59d881db3bc7648
Miguel Munoz <memg92@gmail.com>
2021-01-17 17:37:01 -0300

    adjusted postcss config to purge unused css in production

--- react-app/postcss.config.js
+++ react-app/postcss.config.js
@@ -1,4 +1,5 @@
 const tailwindcss = require("tailwindcss");
 module.exports = {
   plugins: [tailwindcss("./tailwind.js"), require("autoprefixer")],
+  purge: ["./src/**/*.html", "./src/**/*.js", "./src/**/*.jsx"],
 };
